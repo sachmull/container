f45b3e9d8b9f155640a171b005c13adf5e83f420
Sebastian Achmuller de acosta <sachmull@1-E-5.42heilbronn.de>
2023-02-03 18:13:54 +0100

    Terminator almost passes, but now insert(range) will fail, pobably bc size is not set after every construction but only once at the end of the function, meaning an exception will invalidate _size

--- vector.hpp
+++ vector.hpp
@@ -155,27 +155,45 @@ namespace ft {
 
 			template <class InputIt>
 			void	assign(InputIt first, typename ft::enable_if<!ft::is_integral<InputIt>::value, InputIt>::type last) {
-				// clear old content
+				// // clear old content
+				// clear();
+
+				// // make sure there is enough space for the new content
+				// size_type	new_size = (size_type)ft::distance(first, last);
+				// reserve(new_size);
+
+				// // insert new content
+				// for (size_type idx = 0; idx < new_size; ++idx, ++first) {
+				// 	// std::cout << "going in\n";
+				// 	// assign_or_construct(_data + idx, *first);
+				// 	// _allocator.construct(_data + idx, *first);
+
+				// 	++_size;
+				// 	if (idx < _size) {
+				// 		std::cerr << "construct: " << _data + idx << " line: " << __LINE__ << std::endl;
+				// 		_allocator.construct(_data + idx, *first);
+				// 	} else {
+				// 		std::cerr << "assign: " << _data + idx << " line: " << __LINE__ << std::endl;
+				// 		_data[idx] = *first;
+				// 	}
+				// }
+
+				// ^ does not work with single pass iterators
+
 				clear();
 
-				// make sure there is enough space for the new content
-				size_type	new_size = (size_type)ft::distance(first, last);
-				reserve(new_size);
+				// store elements in buffer to avoid problems with single pass iterators
+				vector	buf;
+				for (; first != last; ++first) {
+					buf.push_back(*first);
+				}
+
+				reserve(buf.size());
 
 				// insert new content
-				for (size_type idx = 0; idx < new_size; ++idx, ++first) {
-					// std::cout << "going in\n";
-					// assign_or_construct(_data + idx, *first);
-					// _allocator.construct(_data + idx, *first);
-
+				for (size_type idx = 0; idx < buf.size(); ++idx) {
+					assign_or_construct(_data + idx, buf[idx]);
 					++_size;
-					if (idx < _size) {
-						// std::cerr << "construct: " << _data + idx << " line: " << __LINE__ << std::endl;
-						_allocator.construct(_data + idx, *first);
-					} else {
-						// std::cerr << "assign: " << _data + idx << " line: " << __LINE__ << std::endl;
-						_data[idx] = *first;
-					}
 				}
 			}
 
@@ -292,16 +310,17 @@ namespace ft {
 				if (_size > 0) {
 					for (ssize_t idx = _size - 1; idx >= pos_idx; --idx) {
 						assign_or_construct(_data + count + idx, _data[idx]);
-						// ++_size;
+						// ++_size; // seg fault
 					}
 				}
 
 				// insert new values
 				for (size_type idx = 0; idx < count; ++idx) {
 					assign_or_construct(_data + pos_idx + idx, value);
-					++_size;
+					// ++_size;
 				}
 
+				_size += count;
 				return iterator(_data + pos_idx);
 			}
 
@@ -329,6 +348,12 @@ namespace ft {
 				if (_size > 0) {
 					for (difference_type idx = _size - 1; idx >= pos_idx; --idx) {
 						assign_or_construct(_data + offset + idx, _data[idx]);
+						// if (offset + idx < _size) {
+						// 	_data[offset + idx] = _data[idx];
+						// } else {
+						// 	_allocator.construct(_data + offset + idx, _data[idx]);
+						// 	++_size;
+						// }
 					}
 				}
 
@@ -336,9 +361,16 @@ namespace ft {
 				iterator	it = buf.begin();
 				for (size_type idx = 0; idx < offset; ++idx) {
 					assign_or_construct(_data + pos_idx + idx, *it);
+					// ++_size;
+					// if (pos_idx + idx < _size) {
+					// 	_data[pos_idx + idx] = *it;
+					// } else {
+					// 	_allocator.construct(_data + pos_idx + idx, *it);
+					// 	++_size;
+					// }
 					++it;
-					++_size;
 				}
+				_size += offset;
 
 				return iterator(_data + pos_idx);
 			}
@@ -432,7 +464,9 @@ namespace ft {
 				}
 			}
 
+			#ifdef assign_or_construct
 			#undef assign_or_construct
+			#endif
 			void	assign_or_construct(pointer ptr, const value_type& value, __attribute__((unused))int line = -1) {
 				// std::cout << "assign_or_construct - start\n";
 				if (ptr - _data < (difference_type)_size) {
